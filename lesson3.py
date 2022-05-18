66cfa791577ea5b418569f619712519e17235825
@sponyo12 <davidspec556@gmail.com>
2022-05-18 06:43:04 +0300

    lesson 2 on integers

---
+++ lesson3.py
@@ -0,0 +1,19 @@
+# My age
+#Intergers are basically numbers
+age =18
+
+# Number of siblings
+number_of_siblings =4
+
+dateOfBirth =5
+year_of_birth =2004
+
+#Strings are letters put together to give words and areput in quotes
+favouriteFood ="pizaa"
+first_name= "David"
+secondName="Spencer"
+schoolAttended= "Ovotes"
+
+print ("I am" +first_name)
+print ("I am" +secondName)
+print ("I am" +str(age) +"year's old")
---
+++ lesson5.py
@@ -0,0 +1,19 @@
+#Integers
+
+
+
+
+
+x = 100
+y = 200
+
+sum_of = x + y
+diff = y - x
+prod = x * y
+quot = y / x
+
+print (" The sum of x and y is" + str(sum_of))
+print ("The difference between x and y" + str(diff))
+print  ("The product of x and y is" +str(prod))
+
+print (" The quotient of x and y is" + str(quot))
