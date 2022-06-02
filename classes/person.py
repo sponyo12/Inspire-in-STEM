3f0d06aab20e755b23dc705caedfab6d07fb615f
@sponyo12 <davidspec580@gmail.com>
2022-06-02 11:00:41 +0300

    lesson on classes

--- classes/person.py
+++ classes/person.py
@@ -5,4 +5,10 @@ class Person:
     def sayHi(self):
         print(f"Hello,My name is {self.name} and I'm {self.age} years old.")    
 person1=Person('David',16) 
-person1.sayHi()       
+person1.sayHi() 
+person2=Person('tom',17)     
+person2.sayHi()
+
+
+
+
