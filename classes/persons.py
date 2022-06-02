dbb0b42f4b9465c6a6c3745d7e23f62b64359146
@sponyo12 <davidspec580@gmail.com>
2022-06-02 10:07:46 +0300

    arramnged files

--- classes/persons.py
+++
@@ -1,7 +0,0 @@
-class Person:
-    def __init__(self,_name,_age):
-        self.name=_name
-        self.age=_age
-    def sayHi(self):
-        print(f"Hello,my name is {self.name} and I'm {self.age} years old")    
-person1=Person('David',16)        
