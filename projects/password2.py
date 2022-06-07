668f3f2d745760a0a75d979df7ecbfc9391bd28f
@sponyo12 <davidspec580@gmail.com>
2022-06-07 09:06:19 +0300

    file operations

--- projects/password2.py
+++ projects/password2.py
@@ -1,3 +1,6 @@
+import random
+
+
 class User:
     def __init__(self,_name,_email,_phoneNo):
         self.name=_name
@@ -7,17 +10,8 @@ class User:
         print(f"{self.name} is this your email {self.email} and is this your phone number {self.phoneNo}?")
 person1=User('David','davidspec580@gmail.com',792879778)
 person1.sayHi()
+user=input("yes or no ?")
 
-
-from random import random
-
-print ("WELCOME TO OUR PASSWORD GENERATOR")
-num_passwords=int(input("how many passwords would you like to have?"))
-length_password =int(input("how many characters will they contain?"))
-print("Here are you passwords:")
-for password in range(num_passwords):
-    password=''
-    for characters in range(length_password):
-        password += random.choice(characters)
-    print(password)
-
+if user =="yes":
+    print ("WELCOME TO OUR PASSWORD GENERATOR")
+    
